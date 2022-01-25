704a6566f2ad98f842334ad8e8b320432c9970b8
adolecumberri <adolecumberri@gmail.com>
2022-01-25 23:53:48 +0100

    actualización de la demo.
    TODO: escribir el read me

--- dist/demo.56710ae6.js
+++ dist/demo.56710ae6.js
@@ -1714,7 +1714,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51860" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57531" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
